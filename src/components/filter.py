ca967c890cde4467b3b504bbd65e6297c03b2918
BrunoHoffmann15 <bruno.siqueira.hoffmann@gmail.com>
2024-11-29 21:57:27 -0300

    feat: adiciona filtros

--- src/components/filter.py
+++ src/components/filter.py
@@ -3,7 +3,6 @@ import cv2 as cv
 def blurFilter(img):
   return cv.GaussianBlur(img, (13, 13), 5, 0)
 
-
 filter_options = {
   'blur': blurFilter
 }
--- src/main.py
+++ src/main.py
@@ -4,18 +4,19 @@ from PIL import Image, ImageTk
 import cv2 as cv
 from components.filter import filter_options;
 
-frame = None
+img_frame = None
 cap = None
 activated_filter = None
 video_running = False
 
 # Função para renderizar imagem
 def render_image():
-    global frame, cap
+    global img_frame, cap
 
     stop_camera()
 
-    img = Image.fromarray(frame)
+    img_frame = apply_filter(img_frame)
+    img = Image.fromarray(img_frame)
     imgtk = ImageTk.PhotoImage(image=img)
 
     canvas.itemconfig(image_container, image=imgtk)
@@ -26,9 +27,10 @@ def stop_camera():
     global cap, video_running
 
     if video_running:
-        video_running = False  # Interrompe o loop do vídeo
+        video_running = False
+
         if cap is not None:
-            cap.release()  # Libera a câmera
+            cap.release()
             cap = None
 
         # Limpa o Canvas
@@ -36,7 +38,7 @@ def stop_camera():
         canvas.image = None
 
 
-def update_frame():
+def update_video_frame():
     global cap, video_running
 
     if video_running:
@@ -44,6 +46,7 @@ def update_frame():
 
         if ret:
             # Converte o frame para RGB
+            frame = apply_filter(frame)
             frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
             frame = cv.resize(frame, (384, 216))  # Ajusta ao tamanho do canvas
 
@@ -54,7 +57,7 @@ def update_frame():
             canvas.image = imgtk  # Referência para evitar garbage collection
 
             # Continua atualizando
-            canvas.after(10, update_frame)
+            canvas.after(10, update_video_frame)
         else:
             cap.release()
 
@@ -72,21 +75,32 @@ def open_camera():
 
     # Atualiza os frames no canvas
     video_running = True
-    update_frame()
+    update_video_frame()
 
 # Função para fazer upload de uma imagem
 def upload_photo():
-    global frame
+    global img_frame
     file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg *.jpeg *.png")])
 
     if file_path:
         img = cv.imread(file_path)
-        frame = cv.cvtColor(img, cv.COLOR_RGB2BGR)
+        img_frame = cv.cvtColor(img, cv.COLOR_RGB2BGR)
         render_image()
 
+
+def apply_filter(frame):
+    if activated_filter:
+        frame = activated_filter(frame)
+    
+    return frame
+
 # Função para aplicar filtros
-def apply_filter(filter_name):
-    print(f"Filtro '{filter_name}' aplicado!")
+def select_filter(filter_name):
+    global activated_filter
+    activated_filter = filter_options.get(filter_name)
+
+    if not video_running:
+        render_image()
 
 # Função para adicionar stickers
 def add_sticker(sticker_name):
@@ -115,10 +129,10 @@ image_container = canvas.create_image(200, 200, anchor=tk.CENTER)
 filters_frame = tk.LabelFrame(root, text="Filtros", padx=5, pady=5)
 filters_frame.grid(row=2, column=0, columnspan=4, pady=10)
 
-filter_names = [f"Filtro {i+1}" for i in range(10)]
+filter_names = filter_options.keys()
 
 for i, filter_name in enumerate(filter_names):
-    btn = tk.Button(filters_frame, text=filter_name, command=lambda f=filter_name: apply_filter(f))
+    btn = tk.Button(filters_frame, text=filter_name, command=lambda f=filter_name: select_filter(f))
     btn.grid(row=i // 5, column=i % 5, padx=5, pady=5)
 
 # Botões para stickers
