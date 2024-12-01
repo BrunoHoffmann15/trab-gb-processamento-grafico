d2be24baed4076b8d8e62f04776237c82f368bb3
JoaoAccorsi <joaovaccorci@gmail.com>
2024-12-01 17:48:21 -0300

    Feat: Initial filters:

--- src/components/filter.py
+++ src/components/filter.py
@@ -1,8 +1,20 @@
 import cv2 as cv
 
+output_path = "Output"
+
 def blurFilter(img):
-  return cv.GaussianBlur(img, (13, 13), 5, 0)
+  blur = cv.GaussianBlur(img, (13, 13), 5, 0)
+  _, binary = cv.threshold(blur, 128, 255, cv.THRESH_BINARY)
+  cv.imwrite(output_path+"/blur.jpg", binary)
+  return blur
+
+def gray(img):
+  gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+  _, binary = cv.threshold(gray, 128, 255, cv.THRESH_BINARY)
+  cv.imwrite(output_path+"/gray.jpg", binary)
+  return gray
 
 filter_options = {
-  'blur': blurFilter
+  'Blur': blurFilter,
+  'Gray': gray
 }
