39c8efb3c7ef373732df26da1c5a343a25798401
belcogo <bel.cogo27@gmail.com>
2024-12-01 19:10:04 -0300

    feat: add 5 filter

--- src/components/filter.py
+++ src/components/filter.py
@@ -12,7 +12,37 @@ def gray(img):
   cv.imwrite(output_path+"/gray.jpg", gray)
   return gray
 
+def blueFilter(img):
+  blue = cv.cvtColor(img, cv.COLOR_BGRA2RGBA)
+  cv.imwrite(output_path+"/blue.jpg", blue)
+  return blue
+
+def neonInverted(img):
+  neonInverted = cv.cvtColor(img, cv.COLOR_RGB2YCrCb)
+  cv.imwrite(output_path+"/neonInverted.jpg", neonInverted)
+  return neonInverted
+
+def greenGlitch(img):
+  greenGlitch = cv.cvtColor(img, cv.COLOR_BGR2HLS_FULL)
+  cv.imwrite(output_path+"/greenGlitch.jpg", greenGlitch)
+  return greenGlitch
+
+def increaseSaturation(img):
+  increaseSaturation = cv.cvtColor(img, cv.COLOR_XYZ2RGB)
+  cv.imwrite(output_path+"/increaseSaturation.jpg", increaseSaturation)
+  return increaseSaturation
+
+def inverted(img):
+  inverted = cv.flip(img, -1)
+  cv.imwrite(output_path+"/teste.jpg", inverted)
+  return inverted
+
 filter_options = {
   'Blur': blurFilter,
-  'Gray': gray
+  'Gray': gray,
+  'Blue': blueFilter,
+  'Neon': neonInverted,
+  'Green Glitch': greenGlitch,
+  '+Sturation': increaseSaturation,
+  'Inverted': inverted,
 }
