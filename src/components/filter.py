9e342ccc93c292b2f1c42cb9e100ead0768467b3
JoaoAccorsi <joaovaccorci@gmail.com>
2024-12-01 18:10:20 -0300

    Feat: Adjust the image save option

--- src/components/filter.py
+++ src/components/filter.py
@@ -4,14 +4,12 @@ output_path = "Output"
 
 def blurFilter(img):
   blur = cv.GaussianBlur(img, (13, 13), 5, 0)
-  _, binary = cv.threshold(blur, 128, 255, cv.THRESH_BINARY)
-  cv.imwrite(output_path+"/blur.jpg", binary)
+  cv.imwrite(output_path+"/blur.jpg", blur)
   return blur
 
 def gray(img):
   gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-  _, binary = cv.threshold(gray, 128, 255, cv.THRESH_BINARY)
-  cv.imwrite(output_path+"/gray.jpg", binary)
+  cv.imwrite(output_path+"/gray.jpg", gray)
   return gray
 
 filter_options = {
